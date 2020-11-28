d9a63f0ad6a3b56835abe96066efeb205669521f
estefania-bitbucket <estefania.otero.garcia@gmail.com>
2020-11-28 19:06:11 +0100

    Add documentation to LogReader class

--- src/common/log_reader.py
+++ src/common/log_reader.py
@@ -4,10 +4,20 @@ import csv
 class LogReader(object):
 
     def __init__(self, file_path: str, read_options: str = 'r'):
+        """
+        Basic log reader constructor
+        :param file_path: the path to the log file
+        :param read_options: default is read only
+        """
         self.file_path = file_path
         self.read_options = read_options
 
     def read_log_lines(self):
+        """
+        Read line by line
+        :return a formatted log line as a dict
+        :rtype: dict
+        """
         with open(self.file_path, self.read_options, newline='') as csvfile:
             reader = csv.DictReader(csvfile,
                                     fieldnames=['timestamp_conn', 'source_host', 'target_host'],
