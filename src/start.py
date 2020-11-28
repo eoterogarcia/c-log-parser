5cbe18597526a1612c4eea9fa7e5f5c9c291cb49
estefania-bitbucket <estefania.otero.garcia@gmail.com>
2020-11-28 22:24:10 +0100

    Add parameter to write results to file
    Modify argument test and function to parse log

--- src/start.py
+++ src/start.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from src.common.log_reader import LogReader
 
 
-def get_args() -> Tuple[str, datetime, datetime, str]:
+def get_args() -> Tuple[str, datetime, datetime, str, str]:
     """
     Get the execution arguments
     :return: Tuple with the values (kpi, levels, category, env, period, execution, personalized_period,
@@ -17,27 +17,38 @@ def get_args() -> Tuple[str, datetime, datetime, str]:
                         required=True)
     parser.add_argument('--end_datetime', type=str, help='End datetime connection, format: "dd/mm/yyyy hh:mm:ss"',
                         required=True)
-    parser.add_argument('--target_hostname', type=str, help='', required=True)
+    parser.add_argument('--target_hostname', type=str, help='The target hostname ', required=True)
+    parser.add_argument('--output', type=str, help='Path of the output file to write parsed results. If not defined, '
+                                                   'results are printed in console', required=False)
     args = parser.parse_args()
 
     datetime_format = '%d/%m/%Y %H:%M:%S'
-    return str(args.log_file), datetime.strptime(args.init_datetime, datetime_format), \
-           datetime.strptime(args.end_datetime, datetime_format), str(args.target_hostname)
+    parameters = (str(args.log_file), datetime.strptime(args.init_datetime, datetime_format),
+                  datetime.strptime(args.end_datetime, datetime_format), str(args.target_hostname),
+                  str(args.output) if args.output else None)
+    return parameters
 
 
-def get_source_host_list(reader_generator: Generator, init_datetime: datetime, end_datetime: datetime, target_host: str):
-    source_host_list = []
+def get_source_host_list(reader_generator: Generator,
+                         init_datetime: datetime,
+                         end_datetime: datetime,
+                         target_host: str,
+                         output: str):
     for record in reader_generator:
         if init_datetime <= record[LogReader.TIMESTAMP_CONNECTION] <= end_datetime and target_host == record[LogReader.TARGET_HOST]:
-            source_host_list.append(record[LogReader.SOURCE_HOST])
-            # todo change! write results to file as soon as they are generated
-    return source_host_list
+            if output:
+                # todo write results to file as soon as they are generated
+                record[LogReader.SOURCE_HOST]
+            else:
+                print(record[LogReader.SOURCE_HOST])
+        elif init_datetime > record[LogReader.TIMESTAMP_CONNECTION]:
+            # time range passed, so abort log parser because lines are sorted by timestamp
+            break
 
 
 if __name__ == '__main__':
-    file_path, init, end, target = get_args()
+    file_path, time_init, time_end, target, output_file = get_args()
 
     log_reader = LogReader('test/data/tiny_log.txt')
     reader = log_reader.read_log_lines()
-    host_list = get_source_host_list(reader, init, end, target)
-    print(host_list)
+    get_source_host_list(reader, time_init, time_end, target, output_file)
--- test/utility_test.py
+++ test/utility_test.py
@@ -13,10 +13,12 @@ class TestParserArguments(unittest.TestCase):
                 return_value=argparse.Namespace(log_file="test/data/tiny_log.txt",
                                                 init_datetime="12/08/2019 21:00:00",
                                                 end_datetime="12/08/2019 23:00:00",
-                                                target_hostname="Dmetri"))
+                                                target_hostname="Dmetri",
+                                                output="path/to/something"))
     def test_get_args(self, mock_args):
-        file_path, init_datetime, end_datetime, target_host = get_args()
+        file_path, init_datetime, end_datetime, target_host, output = get_args()
         self.assertEqual(file_path, "test/data/tiny_log.txt", "File path not equal")
         self.assertEqual(init_datetime, datetime(2019, 8, 12, 21, 0), "Init time not equal")
         self.assertEqual(end_datetime, datetime(2019, 8, 12, 23, 0), "End time not equal")
         self.assertEqual(target_host, "Dmetri", "Host not equal")
+        self.assertEqual(output, "path/to/something", "Output not equal")
